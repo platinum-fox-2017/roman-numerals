bc114fdeb50d5e14738be9ea2bba8179f7be5c9f
Agung Prabowo <33645449+agungcaproex@users.noreply.github.com>
2018-02-05 17:29:58 +0700

    upload roman-numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,32 @@
+//Roman Numerals - Challenge
+/*
+- definisikan angka arabic ke roman :
+1 --> I
+5 --> V
+10 --> X
+50 --> L
+100 --> C
+500 --> D
+1000 --> M
+- kemudian masukkan masing-masing tersebut ke dalam sebuah variabel, dan isinya array.
+- 
+*/
+
 function to_roman (num) {
-  // your implementation code here
+  var arabic = num;
+  var roman = [["M",1000],["D",500],["CD",400],["C",100],["L",50],["XL",40],["X",10],["IX",9],["V",5],["IV",4],["I",1]];
+  var hasil = "";
+  
+  for(var i=0; i<roman.length; i++){
+	  var ulang = Math.floor(arabic / roman[i][1]);
+	  while(arabic >= roman[i][1]){
+		  hasil += roman[i][0].repeat(ulang);
+		  arabic -= (roman[i][1] * ulang);
+	  }
+  }
+  return hasil;
+  
+  
 }
 
 // Drive code
