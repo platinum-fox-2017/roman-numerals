b1a8d38536e82be2c190a4ea54663c98757c73c5
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-05 13:10:42 +0700

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,15 @@
-function to_roman (num) {
-  // your implementation code here
+const num = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+function to_roman(value) {
+  result = ''
+  for (let i = 0; i < num.length; i++) {
+    while(value >= num[i]){
+      result += roman[i];
+      value -= num[i];
+    }
+  }
+  return result;
 }
 
 // Drive code
