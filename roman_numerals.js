a307cddab296f4748258759fc99cb215e2866378
hacktiv8 <hacktiv8@H8JKT009.local>
2018-02-05 15:07:15 +0700

    bismillah

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,33 +1,15 @@
 function to_roman (num) {
-  // your implementation code here
-  var arrNum = num.toString().split("").reverse();
-  var romanNum = [];
-  var romanArray = ["I","V","X","L","C","D","M"];
-  for(var i = 0; i < arrNum.length; i++){
-    //console.log("ini: "+arrNum[i])
-    switch(parseInt(arrNum[i])){
-      case 1:
-      case 2:
-      case 3:
-      romanNum.push(romanArray[i*2].repeat(arrNum[i]));
-      break;
-      case 4:
-      romanNum.push(romanArray[i*2]+romanArray[i*2+1]);
-      break;
-      case 5:
-      romanNum.push(romanArray[i*2+1]);
-      break;
-      case 6:
-      case 7:
-      case 8:
-      romanNum.push(romanArray[i*2+1]+romanArray[i*2].repeat(arrNum[i]-5));
-      break;
-      case 9:
-      romanNum.push(romanArray[i*2]+romanArray[i*2+2]);
-      break;
+  const numeral = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  const roman = ['M','DM','D','CD','C','LC','L','XL','X','VX','V','IV','I']
+  let result = ''
+  for(let i = 0 ; i < numeral.length ; i ++){
+    if(num >= numeral[i]){
+      result += roman[i]
+      num -= numeral[i]
+      i = -1
     }
   }
-  return romanNum.reverse().join("");
+  return result
 }
 
 // Drive code
