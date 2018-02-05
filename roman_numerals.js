44558cf525291d8615bfc0b5aca277c9936861b4
Hacktiv8 <hacktiv8@H8JKT003.local>
2018-02-05 15:53:56 +0700

    roman numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,14 @@
 function to_roman (num) {
-  // your implementation code here
+  var arab = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+  var rom = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M']
+  var hasil = '';
+  for(var i=arab.length-1; i>=0; i--){
+    while(num>=arab[i]){
+      num -= arab[i];
+      hasil += rom[i];
+    }
+  }
+  return hasil;
 }
 
 // Drive code
