814738b94d914fa49d4f550427b19de1f9cf716f
srohimah <sitirohimach@gmail.com>
2018-02-05 16:43:12 +0700

    roman numeral using array of object

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,15 +1,68 @@
 function to_roman (num) {
+/*
   let numeral=''
-    let roman = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    let arabic = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+    let arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
     
     for(let i=0; i<roman.length; i++){
-        while(num>=roman[i]){
-            numeral+=arabic[i];
-            num-=roman[i];
+        while(num>=arabic[i]){
+            numeral+=roman[i];
+            num-=arabic[i];
         }
     }
     return numeral;
+*/
+
+//using array of object
+
+    let objnumeral =[{
+            arabic: 1000,
+            roman:'M'
+        },{
+            arabic: 900,
+            roman:'CM'
+        },{
+            arabic: 500,
+            roman:'D'
+        },{
+            arabic: 400,
+            roman:'CD'
+        },{
+            arabic: 100,
+            roman:'C'
+        },{
+            arabic: 90,
+            roman:'XC'
+        },{
+            arabic: 50,
+            roman:'L'
+        },{
+            arabic: 40,
+            roman:'XL'
+        },{
+            arabic: 10,
+            roman:'X'
+        },{
+            arabic: 9,
+            roman:'IX'
+        },{
+            arabic: 5,
+            roman:'V'
+        },{
+            arabic: 4,
+            roman:'IV'
+        },{
+            arabic: 1,
+            roman:'I'
+        },]
+    let result=''
+    for(let i=0; i<objnumeral.length; i++){
+        while(num>=objnumeral[i].arabic){
+            result+=objnumeral[i].roman
+            num-=objnumeral[i].arabic
+        }
+    }
+    return result
 }
 
 // Drive code
