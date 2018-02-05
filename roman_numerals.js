609da4b4573d65a5304710a0ef712fe1a819752a
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-05 14:23:17 +0700

    Change Arabic Numerial to Roman Numerial -- Haidar

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,20 @@
 function to_roman (num) {
   // your implementation code here
+   // huruf Roman diurutkan dari yang terbesar ke yang terkecil
+   var hurufRoman = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+   var roman = '';
+   var i;
+   //looping terhadap isi dari object hurufRoman
+  for ( i in hurufRoman ) {
+    //selama nilai num lebih besar dari nilai object hurufRoman yang terpilih
+    while ( num >= hurufRoman[i] ) {
+      //masukkan nama object ke variable roman
+      roman += i;
+      //kurang num dengan nilai dari hurufRoman yang terpilih
+      num -= hurufRoman[i];
+    }
+  }
+  return roman;
 }
 
 // Drive code
