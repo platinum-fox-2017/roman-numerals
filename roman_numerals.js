51fcd39f9a67163d6f1daa38e05876aaa569f1e8
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-05 17:22:53 +0700

    Optimasi code sebelumnya

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,70 +1,21 @@
 function to_roman (num) {
-  var temp = num.toString();
-  var balikan = '';
+  var angkaRomawi = [['M',1000], ['D',500] , ['CD', 400],['C',100], ['L',50], ['XL',40], ['X',10], ['IX',9] ,['V',5], ['IV',4], ['I',1]];
+  var strBalikan  = '';
 
-  var temp2 = temp.split('');
-  var balikan = '';
-  
-  while (temp2.length > 0) {
-    var observe = temp2[0];
-    if (temp2.length == 4) {
-      for(var i = 0; i < parseInt(observe); i++) {
-        balikan = balikan + 'M';
-      }
-    } else if (temp2.length == 3) {
-      if (parseInt(observe) > 5) {
-        balikan = balikan + 'D';
-        for (var i = 5; i < parseInt(observe); i++) {
-          balikan = balikan + 'C';
-        }
-      } else if (parseInt(observe) != 0){
-        for (var i = parseInt(observe); i < 5; i++) {
-          balikan = balikan + 'C';
-        }
-        balikan = balikan + 'D';
-      }
-    } else if (temp2.length == 2) {
-      if (parseInt(observe) > 5) {
-        balikan = balikan + 'L';
-        for (var i = 5; i < parseInt(observe); i++) {
-          balikan = balikan + 'X';
-        }
-      } else if (parseInt(observe) != 0) {
-        if (parseInt(observe) <= 3) {
-          for (var i = 0; i < parseInt(observe); i++) {
-            balikan = balikan + 'X';
+  while (num > 0) {
+      var angkaTerbesar = 0;
+      for(var i = 0; i < angkaRomawi.length; i++) {
+          if(num >= angkaRomawi[i][1]) {
+              if (angkaRomawi[i][1] > angkaTerbesar) {
+                  angkaTerbesar = angkaRomawi[i][1]
+                  strBalikan = strBalikan + angkaRomawi[i][0];
+              }
           }
-        } else {
-          for (var i = parseInt(observe); i < 5; i++) {
-            balikan = balikan + 'X';
-          }
-          balikan = balikan + 'L';
-        }
       }
-    } else if (temp2.length == 1) {
-      if (parseInt(observe) == 9) {
-        balikan = balikan + 'IX';
-      }
-      else if (parseInt(observe) > 5) {
-        balikan = balikan + 'V';
-        for (var i = 5; i < parseInt(observe); i++) {
-          balikan = balikan + 'I';
-        }
-      } else if (parseInt(observe) > 3 && parseInt(observe) < 5){
-        for (var i = parseInt(observe); i < 5; i++) {
-          balikan = balikan + 'I';
-        }
-        balikan = balikan + 'V';
-      } else if (parseInt(observe) != 0) {
-        for (var i = 0; i < parseInt(observe); i++) {
-          balikan = balikan + 'I';
-        }
-      }
-    }
-    temp2.shift();
+      num = num - angkaTerbesar;        
   }
 
-  return balikan;
+  return strBalikan;
 }
 
 // Drive code
@@ -76,4 +27,3 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
-console.log(to_roman(3000));
