8fd50c3107a900fdaf4a6e0b81d0f9f6a2425187
marcosumali <marco.sumali90@gmail.com>
2018-02-05 18:47:16 +0700

    add file hasil latian susah payah astagfiruloh

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,68 @@
+/*
+input: numbers 1 - 3000
+process:
+output: strings
+
+*/
+
 function to_roman (num) {
   // your implementation code here
+  // console.log(String(num)[2]); // 1646 -> 4
+  // console.log(String(num).length); // 1
+  
+  var hasilRoman = '';
+
+  var arrRoman = [
+    { angka: 1,
+      roman: 'I'},
+    { angka: 4,
+      roman: 'IV'},
+    { angka: 5,
+      roman: 'V'},
+    { angka: 9,
+      roman: 'IX'},
+    { angka: 10,
+      roman: 'X'},
+    { angka: 40,
+      roman: 'XL'},
+    { angka: 50,
+      roman: 'L'},
+    { angka: 90,
+      roman: 'XC'},
+    { angka: 100,
+      roman: 'C'},
+    { angka: 400,
+      roman: 'CD'},
+    { angka: 500,
+      roman: 'D'},
+    { angka: 900,
+      roman: 'CM'},
+    { angka: 1000,
+      roman: 'M'}
+  ]
+
+
+while (num != 0) {
+  var angkaMax = arrRoman[0].angka; // 
+  var indexMax = 0;
+  for (var i = 0; i < arrRoman.length; i++) { // 13 // untuk cari angka terdekat
+    if (arrRoman[i].angka <= num && arrRoman[i].angka > angkaMax) {
+      angkaMax = arrRoman[i].angka;
+      indexMax++;
+    }
+  }
+
+  // console.log(angkaMax); // 100
+  // console.log(indexMax); // 8
+  // console.log(arrRoman[indexMax].roman); // C
+
+  hasilRoman += arrRoman[indexMax].roman;
+  num -= arrRoman[indexMax].angka;
+  
+}
+
+return hasilRoman;
+
 }
 
 // Drive code
